121b55c80c053fb913d6367c359ee5e977c86d1e
cloudstryder <rohinsshanker@gmail.com>
2021-09-16 21:54:28 -0700

    updating placeholder candidates, repairing algorithm

--- scripts/results.js
+++ scripts/results.js
@@ -1,21 +1,22 @@
 /*
 Dict contains all potential results a person can score. update w correct axiums and zips accordingly
 */
+// order of axioms: [pwt, ee, si, pb, h]
 
-canidateResults = [
+candidateResults = [
   {
     "name": "Joe Biden", 
     "image":"https://www.whitehouse.gov/wp-content/uploads/2021/04/P20210303AS-1901-cropped.jpg", 
-    "text":"Text goes here.", 
+    "text":"Joseph Robinette Biden Jr. (born November 20, 1942) is an American politician who is the 46th and current president of the United States. A member of the Democratic Party, he served as the 47th vice president from 2009 to 2017 under Barack Obama and represented Delaware in the United States Senate from 1973 to 2009. (via wikipedia)", 
     "website": "https://joebiden.com/presidency-for-all-americans/", 
-    "zip": "12345", 
-    "avgAxis": "-2", 
-    "election":"election text goes here."
+    "zip": "00000", 
+    "avgAxis": "-2", //will be replaced with "axioms":[pwt, ee, si, pb, h]
+    "election":"2016 Presidential Race"
   },
-  {"name": "Hillary Clinton", "image":"https://upload.wikimedia.org/wikipedia/commons/e/ec/Hillary_Clinton_by_Gage_Skidmore_4_%28cropped%29.jpg", "text":"Text goes here.", "website": "https://www.hillaryclinton.com/", "zip": "00000", "avgAxis": "-1.9", "election":"election text goes here."},
-  {"name": "Bernie Sanders", "image":"https://cdn.britannica.com/51/132851-050-D6CA13B6/Bernie-Sanders-2007.jpg", "text":"Text goes here.", "website": "google.com", "zip": "00000", "avgAxis": "-6.8", "election":"election text goes here."},
-  {"name": "Donald Trump", "image":"https://upload.wikimedia.org/wikipedia/commons/5/56/Donald_Trump_official_portrait.jpg", "text":"Text goes here.", "website": "https://www.donaldjtrump.com/", "zip": "00000", "avgAxis": "8.9", "election":"election text goes here.", "election":"election text goes here."},
-  {"name": "Mitt Romney", "image":"https://upload.wikimedia.org/wikipedia/commons/thumb/7/7f/Mitt_Romney_official_US_Senate_portrait.jpg/1200px-Mitt_Romney_official_US_Senate_portrait.jpg", "text":"Text goes here.", "website": "https://www.romney.senate.gov/", "zip": "00000", "avgAxis": "2.5", "election":"election text goes here."},
-  {"name": "Ron DeSantis", "image":"https://upload.wikimedia.org/wikipedia/commons/b/b3/Ron_DeSantis_2020_%28cropped%29.jpg", "text":"Ron DeSantis (Republican Party) is the 46th governor of Florida. He assumed office on January 8, 2019. His current term ends on January 3, 2023.", "website": "https://www.flgov.com/", "zip": "00000", "avgAxis": "5.3", "election":"election text goes here."}
+  {"name": "Hillary Clinton", "image":"https://upload.wikimedia.org/wikipedia/commons/e/ec/Hillary_Clinton_by_Gage_Skidmore_4_%28cropped%29.jpg", "text":"Hillary Diane Rodham Clinton (born October 26, 1947) is an American politician, diplomat, lawyer, writer, and public speaker who served as the 67th United States Secretary of State from 2009 to 2013, as a United States Senator from New York from 2001 to 2009, and as first lady of the United States from 1993 to 2001, as the wife of President Bill Clinton. (via wikipedia)", "website": "https://www.hillaryclinton.com/", "zip": "00000", "avgAxis": "-1.9", "election":"2016 Presidential Race"},
+  {"name": "Bernie Sanders", "image":"https://cdn.britannica.com/51/132851-050-D6CA13B6/Bernie-Sanders-2007.jpg", "text":"Bernard Sanders (born September 8, 1941) is an American politician and activist who has served as the junior United States senator from Vermont since 2007 and as U.S. Representative for the state's at-large congressional district from 1991 to 2007. (via wikipedia)", "website": "https://berniesanders.com/", "zip": "00000", "avgAxis": "-6.8", "election":"2016 Presidential Race"},
+  {"name": "Donald Trump", "image":"https://upload.wikimedia.org/wikipedia/commons/5/56/Donald_Trump_official_portrait.jpg", "text":"Donald John Trump (born June 14, 1946) is an American politician, media personality, and businessman who served as the 45th president of the United States from 2017 to 2021. (via wikipedia)", "website": "https://www.donaldjtrump.com/", "zip": "00000", "avgAxis": "8.9", "election":"election text goes here.", "election":"2016 Presidential Race"},
+  {"name": "Mitt Romney", "image":"https://upload.wikimedia.org/wikipedia/commons/thumb/7/7f/Mitt_Romney_official_US_Senate_portrait.jpg/1200px-Mitt_Romney_official_US_Senate_portrait.jpg", "text":"Willard Mitt Romney (born March 12, 1947) is an American politician and businessman who has served as the junior United States senator from Utah since January 2019, succeeding Orrin Hatch.  (via wikipedia)", "website": "https://www.romney.senate.gov/", "zip": "00000", "avgAxis": "2.5", "election":"2012 Presidential Race"},
+  {"name": "Ron DeSantis", "image":"https://upload.wikimedia.org/wikipedia/commons/b/b3/Ron_DeSantis_2020_%28cropped%29.jpg", "text":"Ron DeSantis (Republican Party) is the 46th governor of Florida. He assumed office on January 8, 2019. His current term ends on January 3, 2023. (via wikipedia)", "website": "https://www.flgov.com/", "zip": "00000", "avgAxis": "5.3", "election":"2018 Governor of Florida Race"}
 
 ]
--- scripts/resultsFunction.js
+++ scripts/resultsFunction.js
@@ -4,43 +4,59 @@ const urlParams = new URLSearchParams(window.location.search);
 // There will be more variables once we get new axis
 var avg_axis = 0;
 var numOfAxis = 0;
+var userAxioms = [];
 
 
-urlParams.forEach(function(param){
+/* IMPORTANT */
+
+urlParams.forEach(function(param){ //calculates average user axis
+  //add user axioms to array
+  userAxioms.push(param)
   if(parseInt(param) < 50) { //this is rlly sloppy will fix later
     // order of params: pwt, ee, si, pb, h
     console.log(param)
-    avg_axis += parseInt(param);
+    avg_axis += parseInt(param); 
     numOfAxis += 1;
   } else {
     usrZip = param;
   }
 });
+avg_axis = avg_axis/numOfAxis; //avg axis of user
+console.log(userAxioms)
 
-avg_axis = avg_axis/numOfAxis;
 
-// Fill sortedCanidates with objs and accuracy key
-var sortedCanidates = [];
-canidateResults.forEach(function(canidate){
-  var diffTemp = Math.abs(canidate["avgAxis"]-avg_axis); //difference 
 
-  //Add accuracy key to dict and push it sortedCanidates list;
-  var canidateTemp = canidate;
-  canidateTemp["accuracy"] = diffTemp;
-  sortedCanidates.push(canidateTemp);
+// Fill sortedCandidates with objs and accuracy key
+var sortedCandidates = [];
+candidateResults.forEach(function(candidate){
+  /* IMPORTANT */
+  if ("avgAxis" in candidate) {
+    var diffTemp = Math.abs(candidate["avgAxis"]-avg_axis); //total difference between candidate and user
+  } else {
+    var diffTemp = 0;
+    for(var i = 0; i < userAxioms.length; i++) {
+      diffTemp += Math.abs()
+    }
+  }
+  //Add accuracy key to dict and push it sortedCandidates list;
+  var candidateTemp = candidate;
+  candidateTemp["accuracy"] = diffTemp;
+  sortedCandidates.push(candidateTemp);
 });
 
-// Sort sortedCanidates by accuracy key
-sortedCanidates.sort(function(a,b) {
+// Sort sortedCandidates by accuracy key
+sortedCandidates.sort(function(a,b) {
     return a.accuracy - b.accuracy;
 });
 
 function filter() {
   var zip = $("#zipfilter").val();
   $(".resultsHolder").html("");
-  sortedCanidates.forEach(function(c){
+  sortedCandidates.forEach(function(c){
     if(c.zip == zip || zip == "") {
-      var percentAccurate = parseInt(100-c["accuracy"]*5);
+      /* IMPORTANT */ 
+      //STILL NEEDS TO BE UPDATED
+      var percentAccurate = parseInt(100-c["accuracy"]*5); //takes error, multiplies by 5, and subtracts from 100
 
       if(percentAccurate > 85) {
         var htmlToAdd = '<div class="card w-75 border-primary"> <div class="card-body"> <h5 class="card-title">'+c["name"]+' <b class="text-primary">'+percentAccurate+'% Match</b></h5> <img class="card-img" src="'+c["image"]+'" alt="Card image cap"> <strong class="card-text">'+c["election"]+'</strong> <p class="card-text">'+c["text"]+'</p> <p>Zip code: '+c["zip"]+'</p> <a href="'+c["website"]+'" class="btn btn-primary">Learn More</a> </div> </div>';
@@ -53,23 +69,37 @@ function filter() {
   });
 }
 
-// Loop through sortedCanidates and append them all to page
+// Loop through sortedCandidates and append them all to page
 window.onload = function() {
 
   if(parseInt(usrZip) > 0) {
     $(".resultContainer").append("<h4 class='text-primary'>Showing localized results</h4>");
   }
 
-  sortedCanidates.forEach(function(c){
+  sortedCandidates.forEach(function(c){
+    /* IMPORTANT */ //still need to change this, percent accurate is the actual % match from difference index
     var percentAccurate = parseInt(100-c["accuracy"]*5);
 
     if(percentAccurate > 85) {
-      var htmlToAdd = '<div class="card w-75 border-primary"> <div class="card-body"> <h5 class="card-title">'+c["name"]+' <b class="text-primary">'+percentAccurate+'% Match</b></h5> <img class="card-img" src="'+c["image"]+'" alt="Card image cap"> <strong class="card-text">'+c["election"]+'</strong> <p class="card-text">'+c["text"]+'</p> <p>Zip code: '+c["zip"]+'</p> <a href="'+c["website"]+'" class="btn btn-primary">Learn More</a> </div> </div>';
+      var htmlToAdd = '<div class="card w-75 border-primary"> <div class="card-body"> <h5 class="card-title">'+c["name"]+' <b class="text-primary">'+percentAccurate+'% Match</b></h5> <img class="card-img" src="'+c["image"]+'" style="padding-left: 15px" alt="Card image cap"> <strong class="card-text">'+c["election"]+'</strong> <p class="card-text" >'+c["text"]+'</p> <p>Zip code: '+c["zip"]+'</p> <a href="'+c["website"]+'" class="btn btn-primary">Learn More</a> </div> </div>';
     } else {
-      var htmlToAdd = '<div class="card w-75"> <div class="card-body"> <h5 class="card-title">'+c["name"]+' <b>'+percentAccurate+'% Match</b></h5> <img class="card-img" src="'+c["image"]+'" alt="Card image cap"> <strong class="card-text">'+c["election"]+'</strong> <p class="card-text">'+c["text"]+'</p> <p>Zip code: '+c["zip"]+'</p> <a href="'+c["website"]+'" class="btn btn-primary">Learn More</a> </div> </div>';
+      var htmlToAdd = '<div class="card w-75"> <div class="card-body"> <h5 class="card-title">'+c["name"]+' <b>'+percentAccurate+'% Match</b></h5> <img class="card-img" src="'+c["image"]+'" style="padding-left: 15px" alt="Card image cap"> <strong class="card-text">'+c["election"]+'</strong> <p class="card-text">'+c["text"]+'</p> <p>Zip code: '+c["zip"]+'</p> <a href="'+c["website"]+'" class="btn btn-primary">Learn More</a> </div> </div>';
     }
     $(".resultsHolder").append(htmlToAdd);
 
   });
 
+}
+
+
+function userPercentageMatch(userAxioms, candidateAxioms) {
+  const AXIOM_RANGE = 20; //total range axiom can take, so in this case from  -10 to 10
+  var totalError = 0;
+  for(const key in Object.keys(userAxioms)) {
+      totalError += Math.abs(userAxioms[key] - candidateAxioms[key]);
+  }
+  var averageError = totalError/Object.keys(userAxioms).length;
+  var percentMatchRaw = averageError/AXIOM_RANGE;
+  var finalPercentMatch = (percentMatchRaw * 100).toFixed(2);
+  return finalPercentMatch;
 }

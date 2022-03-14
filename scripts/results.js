7c7a3b9e74979dc9f21c80d5a741866f359d5ced
cloudstryder <rohinsshanker@gmail.com>
2022-03-14 12:21:19 -0700

    Update results.js

--- scripts/results.js
+++ scripts/results.js
@@ -38,16 +38,30 @@ candidateResults = [
 
 // zip:San Jose (san jose mayoral election)
 {
-  "name": "Matt Mahan", 
-  "image":"https://sanjosespotlight.s3.us-east-2.amazonaws.com/wp-content/uploads/2020/01/19202039/mm-4.jpg", 
-  "text":"Matt Mahan ran for city council in 2020 because he knew it was time to give local government a “wake-up call.” Between a crisis of homelessness, crime, dirty streets and dysfunctional government, Matt knows we deserve better – and we can do better.", 
-  "links": {
-    "Campaign Website":"https://mahanforsanjose.com/about/"
-  }, 
-  "zip": ["San Jose"], 
-  "axioms":[3, 0, 2, 7, -1],
-  "election":"2022 San Jose Mayoral Race"
+  "name": "Matt Mahan", "image":"https://sanjosespotlight.s3.us-east-2.amazonaws.com/wp-content/uploads/2020/01/19202039/mm-4.jpg", "text":"Matt Mahan ran for city council in 2020 because he knew it was time to give local government a “wake-up call.” Between a crisis of homelessness, crime, dirty streets and dysfunctional government, Matt knows we deserve better – and we can do better.", 
+  "links": { "Campaign Website":"https://mahanforsanjose.com/about/" }, "zip": ["San Jose"], "axioms":[3, 0, 2, 7, 3], "election":"2022 San Jose Mayoral Race" 
 },
+
+{
+  "name": "Cindy Chavez", "image":"https://santaclaralafco.org/sites/default/files/headshots/lafco_headshot_c_chavez.png", "text":"Cindy Chavez is a lifelong resident of San Jose. Her father was a carpenter and her mother worked as a teacher's aide, spending much of her free time volunteering in the community. Cindy attended San Jose State University, where her love of politics and commitment to community led her to a career in public service.", 
+  "links": {"Campaign Website":"http://cindychavezformayor.com",}, "zip": ["San Jose"], "axioms":[-2,-1,-1, 5, -3],"election":"2022 San Jose Mayoral Race"
+},
+
+{
+  "name": "Devora “Dev” Davis", "image":"https://images.squarespace-cdn.com/content/v1/5c9e979f8dfc8c337212615f/1564006040882-B7YN091BZW0ASYSQAB00/cm%2Bdavis%2Bheadshot%2Bhi%2Bres.jpg?format=500w", "text":"Councilmember Dev Davis represents District 6 in San José. Before being elected in 2016, Councilmember Davis spent 12 years as an education researcher for Stanford University's Center for Research on Education Outcomes (CREDO). With a strong interest in community organizing, she served as a Chair of the City of San José's Early Care and Education Committee, a delegate to the Junior League of California’s State Public Affairs Committee, President of the North Willow Glen Neighborhood Association, and Public Relations Chair for the Sherman Oaks Neighborhood Playground Committee. Dev has a bachelor's degree in Economics and master's degrees in Public Policy and Education Policy, Organization & Leadership from Stanford University. ", 
+  "links": {"website title":"https://devdavis.com/" }, "zip": ["San Jose"], "axioms":[-3, -2, 1, 5, 7],"election":"2022 San Jose Mayoral Race"
+},
+
+{
+  "name": "Jonathan Royce Esteban", "image":"https://sanjosespotlight.s3.us-east-2.amazonaws.com/wp-content/uploads/2021/06/23175338/Esteban-2022-Democrat-e1624578809796.png", "text":"As Mayor of San Jose, Esteban promises to improve city employee experiences, enhance city beautification, and fight for equitable wages for working people in the city. Esteban will be a mayor that puts working families and small businesses first. A champion of the ambitious progressive agenda “the Green New Deal for San Jose,” Esteban seeks to rapidly increasing urban housing developments, generate more tax revenue for city services with a slate of green infrastructure jobs, adequately staff up our emergency agencies, and demand healthcare for all.", 
+  "links": {"website title":"https://vote4esteban.com/",}, "zip": ["San Jose"], "axioms":[-7, -8, -5, -4, -7],"election":"2022 San Jose Mayoral Race"
+},
+
+{
+  "name": "Raul Peralez", "image":"https://i0.wp.com/sjd3.com/wp-content/uploads/2021/03/Raul-Peralez-Headshot-.jpg?resize=223%2C257&ssl=1", "text":"As Mayor, Raul will set a course for San Jose that creates comprehensive solutions to address our homeless crisis; build housing that is affordable to working families who call San Jose their home; work with the police and our entire criminal justice system to eradicate systemic racism and keep our communities safe, goals that should and can work hand in hand; continue his work to help our struggling small businesses get back on their feet; bring businesses of all sizes to the table as real partners committed to creating a San Jose that is economically thriving and resilient; and building back up a connected, affordable, dependable public transportation network.", 
+  "links": { "Campaign Website":"https://www.raulperalez.com/", "Ballotpedia":"https://ballotpedia.org/Raul_Peralez"}, "zip": ["San Jose"], "axioms":[-6, -3, -6, -2, -3], "election":"2022 San Jose Mayoral Race"
+},
+
 
 
 
